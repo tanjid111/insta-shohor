73a146cbf85a515ce90f26a5c198f20fbc476620
tanjid111 <tanjidhossain@gmail.com>
2022-03-10 21:30:42 -0800

    error-3

--- js/app.js
+++ js/app.js
@@ -22,7 +22,7 @@ const addToLiked = (id) => {
 };
 
 const reportPost = (id) => {
-  reportedPostsId.push(id);
+  reportedPostsId(id);
   const remainingPosts = posts.filter((post) => !reportedPostsId.includes(post.id));
   showPosts(remainingPosts);
 };
@@ -121,9 +121,9 @@ const createPost = (post) => {
                   <div class="post__description">
                     <small>
                       <a class="post__name--underline" href="#">
-                          ${post.comments?.user}
+                          ${post.comments[0]?.user}
                       </a>
-                      ${post.comments?.text}
+                      ${post.comments[0]?.text}
                     </small>
                   </div>
                   <span class="post__date-time">30 minutes ago</span>
