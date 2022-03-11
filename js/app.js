42a2719de98366f1a66c0f3a371a7562cc7f61ba
tanjid111 <tanjidhossain@gmail.com>
2022-03-10 21:30:12 -0800

    error-2

--- js/app.js
+++ js/app.js
@@ -121,9 +121,9 @@ const createPost = (post) => {
                   <div class="post__description">
                     <small>
                       <a class="post__name--underline" href="#">
-                          ${post.comments[0]?.user}
+                          ${post.comments?.user}
                       </a>
-                      ${post.comments[0]?.text}
+                      ${post.comments?.text}
                     </small>
                   </div>
                   <span class="post__date-time">30 minutes ago</span>
